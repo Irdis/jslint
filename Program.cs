c5c08dffbad3696b8f5a1bb2c81ac7bb9c561cf3
INovitski <INovitski@hazeltree.com>
2025-02-12 13:15:55 +0000

    hello world

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+using System.Text.Json;
+
+using var input = Console.OpenStandardInput();
+using var output = Console.OpenStandardOutput();
+using var writer = new Utf8JsonWriter(output, new JsonWriterOptions {
+    Indented = true
+});
+
+var document = JsonDocument.Parse(input);
+
+document.WriteTo(writer);
+
